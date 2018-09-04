5f25354aabefe2b4ee7ab2d49a81574b2ecc1fd8
Piorek32 <piotr.obrebowski@gmail.com>
2018-09-04 19:48:17 +0200

    initial

--- hamburger.js
+++ hamburger.js
@@ -1,10 +1,11 @@
 
 
 var hamburger = document.querySelector('.hamburger')
-
+var hamburgerNav = document.querySelector('.hamburger-nav')
 
 function active() {
     this.classList.toggle('is-active')
+    hamburgerNav.classList.toggle('add')
 }
 
 hamburger.addEventListener('click', active)
