df8f703dfa9812e450778b357499e31d59e1c401
Piorek32 <piotr.obrebowski@gmail.com>
2019-06-06 15:16:47 +0200

    comit

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ var contactBtn = document.querySelectorAll('.Contact')
 var aboutBtn = document.querySelectorAll('.aboutME')
 var projectBtn = document.querySelectorAll('.project')
 var homeBtn = document.querySelectorAll('.homeBtn')
-console.log(projectBtn)
+
 var stages = document.querySelectorAll('[id^=stage]')
+console.log(stages)
 var aboutStage = document.querySelector('#stage-aboutME')
 var contactStage = document.querySelector('.contact-content')
 var projectStage = document.querySelector('.project-content')
 var homeStage = document.querySelector('.hero')
-
+var singleProjectStage = document.querySelector('#stage-singleProject')
+var mobileAppLink = document.querySelector('#mobileApp')
 
 var trigelDown = document.querySelector('#triangle-down')
 var trigelUP = document.querySelector('#triangle-up')
@@ -28,10 +30,11 @@ function activeHome() {
 }
 startBtn.addEventListener('click', activeHome)
 
-function removeStages() {
+async function removeStages() {
+    
+    stages.forEach(stage => stage.style.transform.scale= "1.2");
+    
     stages.forEach(stage => stage.classList.remove('add'));
-    stages.forEach(stage => stage.style.transform.scale= "1,2");
-
 
 }
 
@@ -49,17 +52,26 @@ function project () {
 }
 function home() {
     heroStage.classList.add('add');
-    contactStage.classList.add('fade')
+   
+}
+function AppStage() {
+    
+    singleProjectStage.classList.add('add');
+   
 }
 
-contactBtn.forEach(btn => btn.addEventListener('click', removeStages))
-contactBtn.forEach(btn => btn.addEventListener('click', contact))
-aboutBtn.forEach(btn => btn.addEventListener('click', removeStages))
-aboutBtn.forEach(btn => btn.addEventListener('click', about))
-projectBtn.forEach(btn => btn.addEventListener('click', removeStages))
-projectBtn.forEach(btn => btn.addEventListener('click', project))
-homeBtn.forEach(btn => btn.addEventListener('click', removeStages))
-homeBtn.forEach(btn => btn.addEventListener('click', home))
+contactBtn.forEach(btn => btn.addEventListener('click', removeStages));
+contactBtn.forEach(btn => btn.addEventListener('click', contact));
+aboutBtn.forEach(btn => btn.addEventListener('click', removeStages));
+aboutBtn.forEach(btn => btn.addEventListener('click', about));
+projectBtn.forEach(btn => btn.addEventListener('click', removeStages));
+projectBtn.forEach(btn => btn.addEventListener('click', project));
+homeBtn.forEach(btn => btn.addEventListener('click', removeStages));
+homeBtn.forEach(btn => btn.addEventListener('click', home));
+
+
+mobileAppLink.addEventListener('click', removeStages);
+mobileAppLink.addEventListener('click', AppStage);
 
 
 
