17a259cb02554b2b4e45cb7097b4c82123c4c714
Piorek32 <piotr.obrebowski@gmail.com>
2019-09-10 11:38:27 +0200

    new page transision

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+
+
+
+const textUp = document.querySelector('h1');
+const textDown = document.querySelector('.title-down');
 var startBtn = document.querySelector('#button')
 var contactBtn = document.querySelectorAll('.contactBtn')
 var aboutBtn = document.querySelectorAll('.aboutBtn')
@@ -19,24 +24,35 @@ var trigelUP = document.querySelector('#triangle-up')
 var heroStage = document.querySelector('.hero')
 var startScreen = document.querySelector('#startscreen')
 function activeHome() {
-    removeStages()
+    removeStages(homeStage)
     trigelDown.classList.toggle('usenand')
     trigelUP.classList.toggle('usenand')
     heroStage.classList.add('krassInefade')
 
-    heroStage.classList.add('add')
+  //  heroStage.classList.add('add')
    startScreen.classList.add('remo')
     this.classList.add('flow')
+   
+    TweenMax.from(textUp,1, { opacity: 0,  left:"45%", delay : 3.5})
+    TweenMax.from(textDown,1, { opacity: 0,  left:"52%", delay : 3.5})
 }
 startBtn.addEventListener('click', activeHome)
 
-async function removeStages() {
-    
-    stages.forEach(stage => stage.style.transform.scale= "1.2");
-    
-    stages.forEach(stage => stage.classList.remove('add'));
+//  function removeStages() {
+//     stages.forEach(stage => stage.classList.remove('add'));
+// }
 
-}
+
+  function removeStages(val) {
+    var tl = new TimelineLite();
+     stages.forEach(stage => {
+         tl.to(stage, 2 ,{left:"-100%"})
+         .to(val, 0.5, { left : 0})
+         .to(val, 1, { scale :(1,1)})
+     });
+ }
+
+
 
 function contact() {
     contactStage.classList.add('add')
@@ -55,9 +71,7 @@ function home() {
    
 }
 function AppStage() {
-    
     singleProjectStage.classList.add('add');
-   
 }
 
 contactBtn.forEach(btn => btn.addEventListener('click', removeStages));
